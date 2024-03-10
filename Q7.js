419e56d3f4727efd677152b8e7af453ea85c5c97
Isra Imran <161215461+IsraImran00@users.noreply.github.com>
2024-03-10 17:13:03 +0500

    Add files via upload

---
+++ Q7.js
@@ -0,0 +1,17 @@
+/*Number Eight: Write addition, subtraction, multiplication and division operations that
+each result in the number 8. Be sure to enclose your operations in print statements to see
+the results.*/
+// Addition
+console.log("Addition:", 6 + 6); // Output: 12
+// Subtraction
+console.log("Subtraction:", 14 - 2); // Output: 12
+// Multiplication
+console.log("Multiplication:", 6 * 2); // Output: 12
+// Division
+console.log("Division:", 24 / 2); // Output: 12
+var a = 15;
+var b = 6;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
---
+++ Q7.ts
@@ -0,0 +1,26 @@
+/*Number Eight: Write addition, subtraction, multiplication and division operations that 
+each result in the number 8. Be sure to enclose your operations in print statements to see 
+the results.*/
+
+// Addition
+console.log("Addition:", 6 + 6); // Output: 12
+
+// Subtraction
+console.log("Subtraction:", 14 - 2); // Output: 12
+
+// Multiplication
+console.log("Multiplication:", 6 * 2); // Output: 12
+
+// Division
+console.log("Division:", 24 / 2); // Output: 12
+
+
+
+let a: number = 15
+let b: number = 6
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
